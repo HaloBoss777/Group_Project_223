fd2c85de1a974a421729d47202157a98df869249
Jeandre van Zyl <Jeandre.transformIt@gmail.com>
2022-08-23 20:34:45 +0200

    Repo Init

---
+++ CarnageApi/CarnageApi.Core/Interfaces/IWantzContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarnageApi.Core.Models;
+
+namespace CarnageApi.Core.Interfaces
+{
+	public class IWantzContext
+	{
+		IRepository<Permission> permission { get; }
+		IRepository<Site> site { get; }
+		IRepository<User> user { get; }
+		IRepository<UserSiteRoll> userSiteRoll { get; }
+	}
+}
---
+++ CarnageApi/CarnageApi/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CarnageApi.Core.Services;
+using CarnageApi.Core.DTOs;
+using CarnageApi.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using CarnageApi.Web.Filters;
+using CarnageApi.Core.Models;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CarnageApi.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public readonly IAuthService _authService;
+
+
+        public AuthController(IAuthService authService) 
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("~/api/Authentication/SignIn")]
+        [AllowAnonymous]
+        public UserAuthDTO SignIn(UserAuthDTO user)
+        {
+            var serviceData = _authService.signIn(user);
+            
+            return serviceData;
+        }
+
+
+        [HttpGet]
+        [Route("~/api/ListUsers")]
+        [AllowAnonymous]
+        public List<User> ListUsers()
+        {
+            Thread.Sleep(5000);
+            return _authService.listUsers();
+        }
+
+        [HttpGet]
+        [Route("~/api/Sites")]
+        [AuthorizationFilter]
+        public List<Site> ListSites()
+        {
+            return _authService.ListSite();
+        }
+
+        [HttpGet]
+        [Route("~/api/Permissions")]
+        [AuthorizationFilter]
+        public List<Permission> ListPermission()
+        {
+            return _authService.ListPermission();
+        }
+
+        [HttpGet]
+        [Route("~/api/UserSiteRoll")]
+        [AuthorizationFilter]
+        public List<UserSiteRollDTO> ListUserSiteRoll()
+        {
+            return _authService.ListUserSiteRoll();
+        }
+    }
+}
