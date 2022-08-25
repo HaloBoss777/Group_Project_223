ecce37c645852ddf6e2a90e1ac0fb5e89d411faf
Jeandre van Zyl <Jeandre.transformIt@gmail.com>
2022-08-25 12:10:58 +0200

    Added Client Model

---
+++ EasterneAdventuresAPI/EasterneAdventuresApi.Core/Models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using EasterneAdventuresApi.Core.DTOs;
+using System.Linq;
+
+namespace EasterneAdventuresApi.Core.Models
+{
+    public class Client : BaseModel
+    {
+        public int Client_Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string CellNum { get; set; }
+        public string RSA_ID { get; set; }
+
+    }
+}
