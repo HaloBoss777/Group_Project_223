d8496d3bb5f262e2e52b3838044d49f1823b120a
HaloBoss777 <dewaldlasiandra@gmail.com>
2022-08-29 15:54:44 +0200

    Added PaymentDTO

---
+++ EasterneAdventuresAPI/EasterneAdventuresApi.Core/DTOs/PaymentDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterneAdventuresApi.Core.DTOs
+{
+    public class PaymentDTO
+    {
+        public int Payment_Id { get; set; }
+        public int Booking_Id { get; set; }
+        public int Client_Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public bool Paid { get; set; }
+    }
+}
