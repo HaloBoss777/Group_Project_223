182180dab9da8b8ce907e2853a2619dedfc0fb14
KBDelta <klasiebotha@gmail.com>
2022-08-29 21:52:13 +0200

    Changed Header Color
    Changed header color to white

--- EasterneAdventuresUIVite/src/pages/Home.vue
+++ EasterneAdventuresUIVite/src/pages/Home.vue
@@ -2,7 +2,7 @@
   <div ref="HomePage" id="HomePage"> 
     <NavBar></NavBar>
     <h1 class="Cover-Text">WELCOME TO ESTERNE ADVENTURES</h1>
-    <h1>THE JOY OF LIFE</h1>
+    <h1 class="Cover-Text">THE JOY OF LIFE</h1>
     <img src="../assets/Esterne.png" alt="">
     <button @click="goToOtherPage" >Click here</button>
   </div>
